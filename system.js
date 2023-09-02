56b0aa32dc6f35fc94b965f6f756e8fcd371f7d9
5-23 <yhanbyeol6@gmail.com>
2023-09-02 16:15:21 +0900

    feat: change some code

--- system.js
+++ system.js
@@ -11,12 +11,13 @@
 variable     : camel case
 enum>element : pascal case
 */
-
-function main() {
+/* fn main { */
+  
   let board = new Board()
-  board.getStr()
-  // console.log(board.getPiece(new Position(0, 8)).move())
-}
+  console.log(board.getStr())
+
+/*} */
+
 
 const ENUM_PIECE = {
   None   : 0,
@@ -33,6 +34,22 @@ const ENUM_TEAM = {
   White : 1,
   Black : 2
 }
+/**
+ * [A, B] 형태로 작성
+ * A = [상하좌우 갈수있는곳 시작 , 상하좌우 갈수있는곳 끝]
+ * B = [대각선 갈수있는곳 시작   , 대각선 갈수있는곳 끝]
+ */
+const moveSystem = {  
+  None   : [[0, 0], [0, 0]],
+  Pawn   : [[1, 2], [0, 0]],
+  Bishop : [[0, 0], [1, 8]],
+  Knight : [[1, 1], [1, 1]],
+  Rook   : [[1, 8], [0, 0]],
+  Queen  : [[1, 8], [1, 8]],
+  King   : [[1, 1], [1, 1]],
+}
+
+
 /**
  * Piece Position
  */
@@ -62,10 +79,28 @@ class Position {
 
 class Board {
   constructor() {
-    /** @type {Array<Piece>} */
-    this.board = new Array(8 * 8);
+    /** @type {Array<Array<Piece>>} */
+    this.board = new Array(8);
     for (let i = 0; i < 8 * 8; i++) this.board[i] = new Piece(ENUM_PIECE.None, ENUM_TEAM.None); 
     
+    this.putPiece(new Piece(ENUM_PIECE.Rook, ENUM_TEAM.White, new Position(1, 1)));
+    this.putPiece(new Piece(ENUM_PIECE.Knight, ENUM_TEAM.White, new Position(2, 1)));
+    this.putPiece(new Piece(ENUM_PIECE.Bishop, ENUM_TEAM.White, new Position(3, 1)));
+    this.putPiece(new Piece(ENUM_PIECE.Queen, ENUM_TEAM.White, new Position(4, 1)));
+    this.putPiece(new Piece(ENUM_PIECE.King, ENUM_TEAM.White, new Position(5, 1)));
+    this.putPiece(new Piece(ENUM_PIECE.Bishop, ENUM_TEAM.White, new Position(6, 1)));
+    this.putPiece(new Piece(ENUM_PIECE.Knight, ENUM_TEAM.White, new Position(7, 1)));
+    this.putPiece(new Piece(ENUM_PIECE.Rook, ENUM_TEAM.White, new Position(8, 1)));
+
+    this.putPiece(new Piece(ENUM_PIECE.Rook, ENUM_TEAM.Black, new Position(1, 8)));
+    this.putPiece(new Piece(ENUM_PIECE.Knight, ENUM_TEAM.Black, new Position(2, 8)));
+    this.putPiece(new Piece(ENUM_PIECE.Bishop, ENUM_TEAM.Black, new Position(3, 8)));
+    this.putPiece(new Piece(ENUM_PIECE.Queen, ENUM_TEAM.Black, new Position(4, 8)));
+    this.putPiece(new Piece(ENUM_PIECE.King, ENUM_TEAM.Black, new Position(5, 8)));
+    this.putPiece(new Piece(ENUM_PIECE.Bishop, ENUM_TEAM.Black, new Position(6, 8)));
+    this.putPiece(new Piece(ENUM_PIECE.Knight, ENUM_TEAM.Black, new Position(7, 8)));
+    this.putPiece(new Piece(ENUM_PIECE.Rook, ENUM_TEAM.Black, new Position(8, 8)));
+
     
     this.board[7*8 + 0] = new Piece(ENUM_PIECE.Rook, ENUM_TEAM.White)
     this.board[7*8 + 7] = new Piece(ENUM_PIECE.Rook, ENUM_TEAM.White)
@@ -100,12 +135,22 @@ class Board {
       this.board[i + 1*8 - 1] = new Piece(ENUM_PIECE.Pawn, ENUM_TEAM.Black)
     }
   }
+  /**
+   * @returns {Number} ENUM_TEAM
+   */
+  isCheck(){
+    return 
+  }
 
   /**
    * @param {Position} position
    */
   getPiece(position) {
     return this.board[(position.x - 1) + (position.y - 1) * 8];
+  }
+
+  putPiece(piece) {
+    this.board[(piece.position.x - 1) + (piece.position.y - 1) * 8] = piece;
   }
 
   getStr() {
@@ -116,13 +161,22 @@ class Board {
         str += '\n'
       }
     }
-    console.log(str)
+    return str;
   }
 }
-
+/**
+ * 
+ */
 class FastBoard {
+  /**
+   * @param {Board} mainBoard 
+   * @param {Board} checkTeam 
+   */
   constructor(mainBoard, checkTeam) {
     this.board = new Array(8 * 8);
+    this.team = checkTeam;
+    this.opponentTeam = checkTeam === ENUM_TEAM.Black ? ENUM_TEAM.White : ENUM_TEAM.Black;
+
     for (let i = 0; i < 8 * 8; i++) {
       if (mainBoard[i].team === ENUM_TEAM.None) {
         this.board[i] = 0;
@@ -136,6 +190,34 @@ class FastBoard {
   check() {
     // 공격 가능한 곳들을 9로  체크하기
 
+    for (let x = 1; x <= 8; x++) {
+      for (let y = 1; y <= 8; y++) {
+        let i = (x - 1) + (y - 1) * 8;
+
+        if (this.board[i] === 0 || this.board[i] === 8) continue;
+
+        const pos = new Position(x, y);
+        const movable = ENUM_PIECE_CBF[this.board[i]](opponentTeam, pos);
+        
+        for (let i = 0; i < 8; i++) {
+          for (let j = 0; j < 8; j++) {
+            
+          }
+        }
+      }
+    }
+  }
+
+  getStr() {
+    let str = ""
+    for (let i in this.board){
+      str += 
+      str += this.board[i].type + " ";
+      if ((Number(i)+1)%8 == 0){
+        str += '\n'
+      }
+    }
+    console.log(str)
   }
 
   getAttack(position) {
@@ -155,8 +237,9 @@ class Piece {
    * @param {Number} type {None: 0, Pawn: 1, Bishop: 2, Knight: 3, Rook: 4, Queen: 5, King: 6}
    * @param {Number} team {white: 0, black: 1}
    * @param {Position} pos {white: 0, black: 1}
+   * @param {Board} board
    */
-  constructor(type, team, pos) {
+  constructor(type, team, pos, board) {
     if (team < 0 || team > 2 || typeof(team) != "number"){ throw new Error("TEAM MATCH ERROR") }
     if (type < 0 || type > 6 || typeof(type) != "number"){ throw new Error("TYPE MATCH ERROR") }
     
@@ -166,6 +249,7 @@ class Piece {
       this.lastMove = 0;
       this.img = `img/${this.#getTeamToChar}-${this.#getNameToChar}.svg`;
       this.pos = pos;
+      this.board = board;
     }
   }
   
@@ -182,6 +266,7 @@ class Piece {
    * @returns {Boolean}
    */
   #canMove(pos) {
+    // this.board.board.
     return false
   }
   
@@ -226,108 +311,102 @@ class Piece {
 // 앙파상
 // 프로모션
 
-
-// 1 이동할 곳이 None 이면
-// 2 적이면 이동
-// 3 아군이 아니고 적일 때까지 지속
-// 4 아군 아니면 그냥 이동
-
 const ENUM_PIECE_CBF = [
-  p => { // None : 0
+  (team, pos) => { // None : 0
     return [];
   },
   
-  p => { // Pawn : 0 
+  (team, pos) => { // Pawn : 0 
     const arr = [];
 
-    if (p.team === ENUM_TEAM.Black) {
-      if (p.pos.y === 2) return arr;
+    if (team === ENUM_TEAM.Black) {
+      if (pos.y === 2) return arr;
 
-      move_2(p.team, p.pos, new Position(1, -1), arr);
-      move_2(p.team, p.pos, new Position(-1, -1), arr);
-      move_1(p.team, p.pos, new Position(0, -1), arr);
+      move_2(team, pos, new Position(1, -1), arr);
+      move_2(team, pos, new Position(-1, -1), arr);
+      move_1(team, pos, new Position(0, -1), arr);
       
-      if (p.lastMove === 0)
-        move_1(p.team, p.pos, new Position(0, -2), arr);
+      if (pos.y === 7)
+        move_1(team, pos, new Position(0, -2), arr);
     }
 
-    if (p.team === ENUM_TEAM.White) {
-      if (p.pos.y === 7) return arr;
+    if (team === ENUM_TEAM.White) {
+      if (pos.y === 7) return arr;
       
-      move_2(p.team, p.pos, new Position(1, 1), arr);
-      move_2(p.team, p.pos, new Position(-1, 1), arr);
-      move_1(p.team, p.pos, new Position(0, 1), arr);
+      move_2(team, pos, new Position(1, 1), arr);
+      move_2(team, pos, new Position(-1, 1), arr);
+      move_1(team, pos, new Position(0, 1), arr);
 
-      if (p.lastMove === 0)
-        move_1(p.team, p.pos, new Position(0, 2), arr);
+      if (pos.y === 2)
+        move_1(team, pos, new Position(0, 2), arr);
     }
 
     return arr;
   },
   
-  p => { // Bishop : 2
+  (team, pos) => { // Bishop : 2
     const arr = [];
 
-    move_3(p.team, p.pos, new Position( 1,  1), arr);
-    move_3(p.team, p.pos, new Position(-1, -1), arr);
-    move_3(p.team, p.pos, new Position(-1,  1), arr);
-    move_3(p.team, p.pos, new Position( 1, -1), arr);
+    move_3(team, pos, new Position( 1,  1), arr);
+    move_3(team, pos, new Position(-1, -1), arr);
+    move_3(team, pos, new Position(-1,  1), arr);
+    move_3(team, pos, new Position( 1, -1), arr);
 
     return arr;
   },  
 
-  p => { // Knight : 3
+  (team, pos) => { // Knight : 3
     const arr = [];
 
-    move_4(p.team, p.pos, new Position(-1,  2), arr);
-    move_4(p.team, p.pos, new Position( 1,  2), arr);
-    move_4(p.team, p.pos, new Position( 2, -1), arr);
-    move_4(p.team, p.pos, new Position( 2,  1), arr);
-    move_4(p.team, p.pos, new Position(-1, -2), arr);
-    move_4(p.team, p.pos, new Position( 1, -2), arr);
-    move_4(p.team, p.pos, new Position(-2, -1), arr);
-    move_4(p.team, p.pos, new Position(-2,  1), arr);
+    move_4(team, pos, new Position(-1,  2), arr);
+    move_4(team, pos, new Position( 1,  2), arr);
+    move_4(team, pos, new Position( 2, -1), arr);
+    move_4(team, pos, new Position( 2,  1), arr);
+    move_4(team, pos, new Position(-1, -2), arr);
+    move_4(team, pos, new Position( 1, -2), arr);
+    move_4(team, pos, new Position(-2, -1), arr);
+    move_4(team, pos, new Position(-2,  1), arr);
 
     return arr;
   },
 
-  p => { // Rook : 4
+  (team, pos) => { // Rook : 4
     const arr = [];
     // move_3rk 뭐하는건가ㅕ?
-    move_3(p.team, p.pos, new Position( 1,  0), arr);
-    move_3(p.team, p.pos, new Position( 0,  1), arr);
-    move_3(p.team, p.pos, new Position(-1,  0), arr);
-    move_3(p.team, p.pos, new Position( 0, -1), arr);
+    move_3(team, pos, new Position( 1,  0), arr);
+    move_3(team, pos, new Position( 0,  1), arr);
+    move_3(team, pos, new Position(-1,  0), arr);
+    move_3(team, pos, new Position( 0, -1), arr);
 
     return arr;
   },
 
-  p => { // Queen : 5
+  (team, pos) => { // Queen : 5
     const arr = [];
 
-    move_3(p.team, p.pos, new Position( 1,  1), arr);
-    move_3(p.team, p.pos, new Position(-1, -1), arr);
-    move_3(p.team, p.pos, new Position(-1,  1), arr);
-    move_3(p.team, p.pos, new Position( 1, -1), arr);
-    move_3(p.team, p.pos, new Position( 1,  0), arr);
-    move_3(p.team, p.pos, new Position( 0,  1), arr);
-    move_3(p.team, p.pos, new Position(-1,  0), arr);
-    move_3(p.team, p.pos, new Position( 0, -1), arr);
+    move_3(team, pos, new Position( 1,  1), arr);
+    move_3(team, pos, new Position(-1, -1), arr);
+    move_3(team, pos, new Position(-1,  1), arr);
+    move_3(team, pos, new Position( 1, -1), arr);
+    move_3(team, pos, new Position( 1,  0), arr);
+    move_3(team, pos, new Position( 0,  1), arr);
+    move_3(team, pos, new Position(-1,  0), arr);
+    move_3(team, pos, new Position( 0, -1), arr);
 
     return arr;
   },
 
-  p => { // King : 6
+  (team, pos) => { // King : 6
     const arr = [];
 
-    move_4(p.team, p.pos, new Position(-1,  1), arr);
-    move_4(p.team, p.pos, new Position( 1,  1), arr);
-    move_4(p.team, p.pos, new Position( 1, -1), arr);
-    move_4(p.team, p.pos, new Position( 1,  1), arr);
-    move_4(p.team, p.pos, new Position(-1, -1), arr);
-    move_4(p.team, p.pos, new Position( 1, -1), arr);
-    move_4(p.team, p.pos, new Position(-1, -1), arr);
-    move_4(p.team, p.pos, new Position(-1,  1), arr);
+    move_4(team, pos, new Position(-1,  1), arr);
+    move_4(team, pos, new Position( 1,  1), arr);
+    move_4(team, pos, new Position( 1, -1), arr);
+    move_4(team, pos, new Position( 1,  1), arr);
+    move_4(team, pos, new Position(-1, -1), arr);
+    move_4(team, pos, new Position( 1, -1), arr);
+    move_4(team, pos, new Position(-1, -1), arr);
+    move_4(team, pos, new Position(-1,  1), arr);
 
     return arr;
   },
@@ -340,37 +419,37 @@ const ENUM_SPECIAL_MOVE = {
 }
 
 const ENUM_PIECE_CBF_SPECIAL = [
-  p => { // Enpassant 0
+  (team, pos) => { // Enpassant 0
     const arr = [];
 
-    if (p.team === ENUM_TEAM.Black) {
+    if (team === ENUM_TEAM.Black) {
 
 
-      move_2(p.team, p.pos, new Position(1, -1), arr);
-      move_2(p.team, p.pos, new Position(-1, -1), arr);
+      move_2(team, pos, new Position(1, -1), arr);
+      move_2(team, pos, new Position(-1, -1), arr);
     }
 
-    if (p.team === ENUM_TEAM.White) {
-      move_2(p.team, p.pos, new Position(1, 1), arr);
-      move_2(p.team, p.pos, new Position(-1, 1), arr);
+    if (team === ENUM_TEAM.White) {
+      move_2(team, pos, new Position(1, 1), arr);
+      move_2(team, pos, new Position(-1, 1), arr);
     }
 
     return arr;
   },
 
-  p => { // Promotion 1 
+  (team, pos) => { // Promotion 1 
     const arr = [];
 
-    if (p.team === ENUM_TEAM.Black && p.pos.y === 2) {
-      move_2(p.team, p.pos, new Position(1, -1), arr);
-      move_2(p.team, p.pos, new Position(-1, -1), arr);
-      move_1(p.team, p.pos, new Position(0, -1), arr);
+    if (team === ENUM_TEAM.Black && pos.y === 2) {
+      move_2(team, pos, new Position(1, -1), arr);
+      move_2(team, pos, new Position(-1, -1), arr);
+      move_1(team, pos, new Position(0, -1), arr);
     }
 
-    if (p.team === ENUM_TEAM.White && p.pos.y === 7) {
-      move_2(p.team, p.pos, new Position(1, 1), arr);
-      move_2(p.team, p.pos, new Position(-1, 1), arr);
-      move_1(p.team, p.pos, new Position(0, 1), arr);
+    if (team === ENUM_TEAM.White && pos.y === 7) {
+      move_2(team, pos, new Position(1, 1), arr);
+      move_2(team, pos, new Position(-1, 1), arr);
+      move_1(team, pos, new Position(0, 1), arr);
     }
 
     return arr;
@@ -384,8 +463,12 @@ const ENUM_PIECE_CBF_SPECIAL = [
 
 
 
+
+// 3 아군이 아니고 적일 때까지 지속
+// 4 아군 아니면 그냥 이동
+
 /**
- * 이동할 곳이 None 이면
+ * 이동할 곳이 기물이 없을 때
  * @param {Number} team 
  * @param {Position} currentPos 
  * @param {Position} movePos 
@@ -399,7 +482,7 @@ const move_1 = (team, currentPos, movePos, inArr) => {
 }
 
 /**
- * 
+ * 이동할 곳에 적이 있을 때
  * @param {Number} team 
  * @param {Position} currentPos 
  * @param {Position} movePos 
@@ -414,7 +497,7 @@ const move_2 = (team, currentPos, movePos, inArr) => {
 }
 
 /**
- * 
+ * 아군이 아니고 있을 때까지
  * @param {Number} team 
  * @param {Position} currentPos 
  * @param {Position} movePos 
@@ -433,6 +516,14 @@ const move_3 = (team, currenPos, movePos, inArr) => {
   }  
 }
 
+
+/**
+ * 아군 아니면 그냥 이동
+ * @param {Number} team 
+ * @param {Position} currentPos 
+ * @param {Position} movePos 
+ * @param {Array<Position>} inArr 
+ */
 const move_4 = (team, currentPos, movePos, inArr) => {
   const pos = currentPos.add(movePos);
   
@@ -440,8 +531,15 @@ const move_4 = (team, currentPos, movePos, inArr) => {
   if (board.getPiece(pos).team !== team) inArr.push(movePos); 
 }
 
+/**
+ * 앙파상
+ * @param {Number} team 
+ * @param {Position} currentPos 
+ * @param {Position} movePos 
+ * @param {Array<Position>} inArr 
+ */
 const move_5 = (team, currentPos, movePos, inArr) => {
-  const y = team === ENUM_TEAM.Black ? 4 : 5;
+  const y = team === ENUM_TEAM.Black ? 4: 5;
 
   if (currentPos.y !== y) return ;
 
@@ -454,5 +552,3 @@ const move_5 = (team, currentPos, movePos, inArr) => {
 
   if (piece.team === opponentTeam && piece.lastMove === board.turn && piece.type === ENUM_PIECE.Pawn) inArr.push(movePos);
 }
-
-main()
